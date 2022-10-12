a6bdaf1c2aabf312bccc6db1f4a5cab7d91ac69f
Jemy <jemy@jemys-mbp.lan>
2022-10-12 10:40:14 -0400

    adding to JS file

--- tts-app/src/App.js
+++ tts-app/src/App.js
@@ -1,5 +1,18 @@
 import './App.css';
 
+const textArea = document.querySelector('#text')
+let voiceList = document.querySelector('#voice')
+let speechBtn = document.querySelector('.submit')
 
+let synth = speechSynthesis
+let isSpeaking = true
+
+function voiceSpeech() {
+    for (let voice of synth.getVoices()) {
+        let option = document.createElement('option')
+        option.text = voice.name
+        voiceList.addEventListener(option)
+    }
+}
 
 export default App;
